3166523cfdbfbfe3cbe5a1ed495ba1dba38ae7a2
Allison Darwin <allisonpalmer07@gmail.com>
2020-05-21 12:17:34 -0400

    add .site-content to center content

--- wp-content/themes/underscores/footer.php
+++ wp-content/themes/underscores/footer.php
@@ -12,19 +12,21 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'underscores-portfolio' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'underscores-portfolio' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'underscores-portfolio' ), 'underscores-portfolio', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
+		<div class="site-content">
+			<div class="site-info">
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'underscores-portfolio' ) ); ?>">
+					<?php
+					/* translators: %s: CMS name, i.e. WordPress. */
+					printf( esc_html__( 'Proudly powered by %s', 'underscores-portfolio' ), 'WordPress' );
+					?>
+				</a>
+				<span class="sep"> | </span>
+					<?php
+					/* translators: 1: Theme name, 2: Theme author. */
+					printf( esc_html__( 'Theme: %1$s by %2$s.', 'underscores-portfolio' ), 'underscores-portfolio', '<a href="http://underscores.me/">Underscores.me</a>' );
+					?>
+			</div><!-- .site-info -->
+		</div><!-- end .site-content -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
