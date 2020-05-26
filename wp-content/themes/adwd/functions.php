5c6634709137421a01f1d7c74a46022822b8f7cd
Allison Darwin <allisonpalmer07@gmail.com>
2020-05-26 15:37:27 -0400

    add functions.php file to theme and adwd_setup function

---
+++ wp-content/themes/adwd/functions.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Allison Darwin functions and definitions
+ *
+ * Set up the theme and provides some helper functions, which are used in the
+ * theme as custom template tags. Others are attached to action and filter
+ * hooks in WordPress to change core functionality.
+ *
+ * When using a child theme you can override certain functions (those wrapped
+ * in a function_exists() call) by defining them first in your child theme's
+ * functions.php file. The child theme's functions.php file is included before
+ * the parent theme's file, so the child theme functions would be used.
+ *
+ * @link http://codex.wordpress.org/Theme_Development
+ * @link http://codex.wordpress.org/Child_Themes
+ *
+ * Functions that are not pluggable (not wrapped in function_exists()) are
+ * instead attached to a filter or action hook.
+ *
+ * For more information on hooks, actions, and filters,
+ * @link http://codex.wordpress.org/Plugin_API
+ *
+ * @package WordPress
+ * @subpackage Allison Darwin
+ * @since Allison Darwin 1.0
+ */
+
+ // Theme support for menus
+function adwd_setup() {
+
+    /*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
+
+    // Register Menus
+    register_nav_menus( array (
+        'top-nav' => __( 'Top Nav', 'adwd' ),
+        'social-media'  => __( 'Social Media Nav', 'adwd' ),
+    ) );
+}
+add_action( 'after_setup_theme', 'adwd_setup' );
