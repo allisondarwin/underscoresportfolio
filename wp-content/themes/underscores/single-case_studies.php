4b2aba2fed10b9e95f49e9e4418e29e430f4b8ec
Allison Darwin <allisonpalmer07@gmail.com>
2020-05-28 17:36:51 -0400

    edit and style single case study page

--- wp-content/themes/underscores/single-case_studies.php
+++ wp-content/themes/underscores/single-case_studies.php
@@ -15,14 +15,14 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
-		<div class="site-content">
+	<main id="primary" class="site-main single-case-study">
+		<div class="site-content hero-content">
 			<?php
 			while ( have_posts() ) :
 				the_post();
 
-				get_template_part( 'template-parts/content', 'page' );
-
+				get_template_part( 'template-parts/content', 'page' );?>
+        <h2><?php the_field('case_study_services'); ?></h2><?php
 				// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) :
 					comments_template();
@@ -32,6 +32,19 @@ get_header();
 			?>
 		</div><!-- end .site-content -->
 	</main><!-- #main -->
+
+  <section class="case-study-content">
+    <div class="site-content">
+      <div class="case-study-text">
+        <h3><span>Client:</span> <?php the_field('case_study_client'); ?></h3>
+        <p><span>The problem:</span> <?php the_field('case_study_problem'); ?></p>
+        <p><span>The solution:</span> <?php the_field('case_study_solution'); ?></p>
+        <p><span>The results:</span> <?php the_field('case_study_results'); ?></p>
+      </div>
+      <div class="case-study-images">
+      </div>
+    </div>
+  </section>
 
 <?php
 
