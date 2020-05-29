64da1c7fb1a35365612eba7a666b02f10399d699
Allison Darwin <allisonpalmer07@gmail.com>
2020-05-29 10:52:50 -0400

    edits to single case study structure and styles

--- wp-content/themes/underscores/single-case_studies.php
+++ wp-content/themes/underscores/single-case_studies.php
@@ -17,9 +17,7 @@ get_header();
 
 	<main id="primary" class="site-main single-case-study">
 		<div class="site-content hero-content">
-			<?php
-			while ( have_posts() ) :
-				the_post();
+			<?php while ( have_posts() ) : the_post();
 
 				get_template_part( 'template-parts/content', 'page' );?>
         <h2><?php the_field('case_study_services'); ?></h2><?php
@@ -33,18 +31,34 @@ get_header();
 		</div><!-- end .site-content -->
 	</main><!-- #main -->
 
-  <section class="case-study-content">
+  <article class="case-study-content">
     <div class="site-content">
       <div class="case-study-text">
         <h3><span>Client:</span> <?php the_field('case_study_client'); ?></h3>
+				<h4><?php the_field('case_study_services'); ?></h4>
         <p><span>The problem:</span> <?php the_field('case_study_problem'); ?></p>
         <p><span>The solution:</span> <?php the_field('case_study_solution'); ?></p>
         <p><span>The results:</span> <?php the_field('case_study_results'); ?></p>
+				<p class="visit-live-site-link"><a href='#' target='_blank'>Visit Live Site</a></p>
       </div>
       <div class="case-study-images">
+				<?php
+					$image_1 = get_field('case_study_image_1');
+					$image_2 = get_field('case_study_image_2');
+					$image_3 = get_field('case_study_image_3');
+					$size = 'full'; ?>
+				<?php	if ($image_1) {
+						echo wp_get_attachment_image($image_1, $size);
+					}?>
+				<?php	if ($image_2) {
+						echo wp_get_attachment_image($image_2, $size);
+					}?>
+				<?php	if ($image_3) {
+						echo wp_get_attachment_image($image_3, $size);
+					}?>
       </div>
     </div>
-  </section>
+  </article>
 
 <?php
 
