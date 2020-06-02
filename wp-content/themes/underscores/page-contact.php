cad37b89b18dceba62fdb9fa3b7262d2600ef9ba
Allison Darwin <allisonpalmer07@gmail.com>
2020-06-02 12:34:51 -0400

    copy page.php over to contact-page.php

---
+++ wp-content/themes/underscores/page-contact.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying the contact page
+ *
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package underscores-portfolio
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+		<div class="site-content">
+			<?php
+			while ( have_posts() ) :
+				the_post();
+
+				get_template_part( 'template-parts/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+		</div><!-- end .site-content -->
+	</main><!-- #main -->
+
+<?php
+
+get_footer();
