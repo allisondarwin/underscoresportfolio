47608c465fb1c75f46dca82bd671a655f00b6db2
Allison Darwin <allisonpalmer07@gmail.com>
2020-06-02 13:28:10 -0400

    add ninja form plugin and style contact form

--- wp-content/themes/underscores/page-contact.php
+++ wp-content/themes/underscores/page-contact.php
@@ -11,7 +11,7 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+	<main id="primary" class="site-main contact-page">
 		<div class="site-content">
 			<?php
 			while ( have_posts() ) :
