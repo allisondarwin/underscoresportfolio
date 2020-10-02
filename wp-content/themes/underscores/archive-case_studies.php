ce5fd11d0504a97702b0fb2a61e9a98c21deb0c1
Allison Darwin <allisonpalmer07@gmail.com>
2020-10-02 12:17:14 -0400

    removed page header from archive

--- wp-content/themes/underscores/archive-case_studies.php
+++ wp-content/themes/underscores/archive-case_studies.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying archive pages
+ * The template for displaying case study archive.
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
@@ -13,13 +13,6 @@ get_header();
 	<main id="primary" class="site-main">
 		<div class="site-content">
 			<?php if ( have_posts() ) : ?>
-
-				<header class="page-header">
-					<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="archive-description">', '</div>' );
-					?>
-				</header><!-- .page-header -->
 
 				<?php
 				/* Start the Loop */
