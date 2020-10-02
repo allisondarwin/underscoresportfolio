60d2ec312b0ef520a4d241d4128af5610c23eb36
Allison Darwin <allisonpalmer07@gmail.com>
2020-10-02 12:40:14 -0400

    added custom fields to template file

--- wp-content/themes/underscores/archive-case_studies.php
+++ wp-content/themes/underscores/archive-case_studies.php
@@ -10,21 +10,31 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+	<main id="primary" class="site-main case-study-archive">
 		<div class="site-content">
 			<?php if ( have_posts() ) : ?>
 
-				<?php
-				/* Start the Loop */
-				while ( have_posts() ) :
-					the_post();
+				<?php while ( have_posts() ) : the_post();
+          $client = get_field('case_study_client');
+					$services = get_field('case_study_services');
+          $image_1 = get_field('case_study_image_1');
+          $size = 'full';?>
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-					get_template_part( 'template-parts/content', get_post_type() );
+          <article class="archive-case-study">
+      		    <aside class="archive-case-study-sidebar">
+            	   <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            	   <h5><?php echo $services; ?></h5>
+							   <?php the_excerpt(); ?>
+							   <p class="read-more-link"><a href="<?php the_permalink(); ?>">View Project &gt;</p></a>
+      		    </aside>
+      		    <div class="archive-case-study-images">
+                  <a href="<?php the_permalink(); ?>">
+							    <?php if($image_1) {
+									echo wp_get_attachment_image($image_1, $size);
+							    } ?>
+                  </a>
+      		    </div>
+			    </article> <?php
 
 				endwhile;
 
