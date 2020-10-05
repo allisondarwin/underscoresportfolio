fdc04b0d436ba2b3e28a962af7e1343f9584db07
Allison Darwin <allisonpalmer07@gmail.com>
2020-10-05 16:56:07 -0400

    activated wf firewall on local host

---
+++ wordfence-waf.php
@@ -0,0 +1,8 @@
+<?php
+// Before removing this file, please verify the PHP ini setting `auto_prepend_file` does not point to this.
+
+if (file_exists('/Users/allisonpalmer/Sites/underscoresportfolio/wp-content/plugins/wordfence/waf/bootstrap.php')) {
+	define("WFWAF_LOG_PATH", '/Users/allisonpalmer/Sites/underscoresportfolio/wp-content/wflogs/');
+	include_once '/Users/allisonpalmer/Sites/underscoresportfolio/wp-content/plugins/wordfence/waf/bootstrap.php';
+}
+?>
